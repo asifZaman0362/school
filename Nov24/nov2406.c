c8803b411639106c94f2279914dabd387e5811c0
asifZaman0362 <zero@362bits.com>
2020-11-24 20:06:22 +0530

    Added files

--- Nov24/nov2406.c
+++ Nov24/nov2406.c
@@ -4,6 +4,6 @@ int main() {
     printf("Enter a character : ");
     scanf("%c", &c);
     if (c < '0' || c > '9') printf("'%c' is not a number.\n", c);
-    printf("%c is a number.\n", c);
+    else printf("%c is a number.\n", c);
     return 0;
 }
