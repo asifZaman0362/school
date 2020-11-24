77fba172560434972d21122fe0699f4e0c8850f4
asifZaman0362 <zero@362bits.com>
2020-11-24 10:01:54 +0530

    added sets

--- sets.py
+++ sets.py
@@ -1,13 +1,36 @@
 import sys
 
-def cartesian_product(A, B):
+def cp(A, B):
     prod = []
     for x in A:
         for y in B:
             prod.append([x, y])
-            
     return prod
     
+
+def dif(A, B):
+    diff = []
+    for x in A:
+        if x not in B: diff.append(x)
+    return diff
+    
+
+def un(A, B):
+    uni = []
+    for i in A:
+        if i not in uni: uni.append(i)
+    for i in B:
+        if i not in uni: uni.append(i)
+    return uni
+
+
+def ins(A, B):
+    ins = []
+    for i in A:
+        if i in B: ins.append(i)
+    return ins
+
+
 def parse_set(x):
     clean_str = ''
     for c in x:
@@ -16,10 +39,27 @@ def parse_set(x):
     return clean_str.split(',')
 
 
-A = parse_set(sys.argv[1])
-B = parse_set(sys.argv[2])
-AxB = cartesian_product(A, B)
-print('{ ', end='')
-for i in AxB:
-    print('({},{}){}'.format(i[0], i[1], ', ' if i != AxB[-1] else ''), end='')
-print(' }')
+def prints(s, nm="S"):
+    print('{} = {{'.format(nm), end='')
+    for i in s:
+        print('{}{}'.format(i, ', ' if i != s[-1] else ''), end='')
+    print(' }')
+
+
+U = [i for i in range(1, 11)]
+A = [i for i in U if i % 2 != 0 or i == U[-1]]
+C = [8, 9, 10]
+B = dif(U, A)
+prints(A, 'A')
+prints(B, 'B')
+prints(C, 'C')
+prints(U, 'U')
+print('\n'*3)
+prints(un(dif(A, B), dif(B, A)), "(A-B)U(B-A)")
+print('\n'*3)
+prints(cp(A, un(B, C)), 'A×(B U C)')
+print('\n'*3)
+prints(cp(B, ins(A, C)), 'B×(A n C)')
+print('\n'*3)
+prints(dif(A, un(B, C)), 'A - (B U C)')
+print('\n'*3)
