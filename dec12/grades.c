48f12d4c233258e9abf52e3730bc0ef2871d75a7
asifZaman0362 <zero@362bits.com>
2020-12-13 18:56:34 +0530

    added grades

---
+++ dec12/grades.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+const char* getGrade(int marks) {
+
+    switch (marks / 5) {
+		case 20:
+        case 19:
+            return "A+";
+        case 18:
+            return "A";
+        case 17:
+            return "B+";
+        case 16:
+            return "B";
+        case 15:
+            return "C+";
+        case 14:
+            return "C";
+        case 13:
+            return "D+";
+        case 12:
+            return "E+";
+        case 11:
+        case 10:
+            return "E";
+        default:
+            return "F";
+    }
+
+    return "F";
+
+}
+
+int main() {
+
+    int marks = 0;
+    char grade;
+
+    //system("clear");
+    while(1) {
+        printf("Enter the marks obtained by the student >> ");
+        scanf("%d", &marks);
+        if (marks >= 0 && marks <= 100) break;
+        else printf("Marks can only be from 0 to 100!\nTry again...");
+    }
+    
+    printf("Student's grade : %s\n", getGrade(marks));
+
+    return 0;
+
+}
