995c8be0e4012b6969989c3b5dc49b09a726dcc2
asifZaman0362 <zero@362bits.com>
2020-12-25 21:33:53 +0530

    added float parsing

--- digital_design/arithmetic.cpp
+++ digital_design/arithmetic.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+
 
 namespace tst {
 
 	std::stack<std::string> numStack;
+	std::vector<std::string> numList;
 	std::stack<char> opStack;
 
 	inline bool isdigit(char c) {
@@ -27,6 +30,13 @@ namespace tst {
 		return c - '0';
 	}
 
+	inline std::string ToString(char c) {
+		std::stringstream stream;
+		stream << c;
+		return stream.str();
+	}
+
+
 	inline float math(int a, int b, char op) {
 		if (op == '+') return a + b;
 		else if (op == '-') return a - b;
@@ -35,24 +45,33 @@ namespace tst {
 		else return 0;
 	}
 
-	inline std::string reverse(std::string str) {
+	inline std::string formatNumbers(std::string str, std::vector<std::string> p_numList) {
+		std::stringstream stream;
+		int j = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (j >= p_numList.size()) break;
+			else if (str[i] == '#') { stream << p_numList[j] << ';'; j++; }
+			else stream << str[i];
+		}
+		return stream.str();
+	}
+
+	inline std::string reverse(std::string str, std::vector<std::string> p_numList) {
+		std::vector<std::string> revVector;
+		for (int i = p_numList.size() - 1; i >= 0; i--) {
+			revVector.push_back(p_numList[i]);
+		}
 		std::stringstream stream;
 		for (int i = str.length()-1; i >= 0; i--) {
 			stream << str.c_str()[i];
 		}
-		return stream.str();
+		return formatNumbers(stream.str(), revVector);
 	}
 
 	inline float pow(float a, int b) {
 		float prod = 1;
 		for (int i = 0; i < b; i++) prod *= a;
 		return prod;
-	}
-
-	inline std::string ToString(char c) {
-		std::stringstream stream;
-		stream << c;
-		return stream.str();
 	}
 
 	float parseNum(std::string str) {
@@ -80,7 +99,8 @@ namespace tst {
 				numstring << cstr[i];
 				std::cout << numstring.str() << std::endl;
 				if (isoperator(cstr[i+1]) || cstr[i+1] == '\0') {
-					numStack.push(numstring.str());
+					numStack.push("#");
+					numList.push_back(numstring.str());
 					numstring.str("");
 				}
 			}
@@ -108,7 +128,7 @@ namespace tst {
 			stream << numStack.top();
 			numStack.pop();
 		}
-		std::string st1 = reverse(stream.str());
+		std::string st1 = reverse(stream.str(), numList);
 		std::stringstream stream2;
 		while (!opStack.empty()) {
 			stream2 << opStack.top();
@@ -125,10 +145,17 @@ namespace tst {
 	float ParsePostfix(std::string exp) {
 		std::stack<char> mainStack;
 		std::stack<float> numStack;
+		std::stringstream numstring;
+
 		for (int i = 0; i < exp.length(); i++) {
-			if (isdigit(exp.at(i))) {
-				numStack.push(todigit(exp.at(i)));
-			} else if (isoperator(exp.at(i))) {
+			if (isdigit(exp[i]) || exp[i] == '.') {
+				numstring << exp[i];
+				std::cout << numstring.str() << std::endl;
+				if (isoperator(exp[i+1]) || exp[i+1] == '\0' || exp[i+1] == ';') {
+					numStack.push(parseNum(numstring.str()));
+					numstring.str("");
+				}
+			} else if (isoperator(exp[i])) {
 				int a = numStack.top();
 				numStack.pop();
 				int b = numStack.top();
@@ -165,9 +192,12 @@ int main() {
 	// num = tst::parseNum(str);
 	// if (num < 0) std::cout << "Failed to parse!" << std::endl;
 	// else std::cout << num << std::endl;
+
 	std::cout << "Enter an expression : ";
 	std::string exp;
 	std::cin >> exp;
 	std::cout << "Parsed : " << tst::ConvertToPostfix(exp) << std::endl;
+	std::cout << "Solved : " << tst::ParsePostfix(tst::ConvertToPostfix(exp)) << std::endl;
+
     return 0;
 }
