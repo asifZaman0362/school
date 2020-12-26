7dfd5b94b5e7c59fb5314f6b9baab46eea9b6c54
asifZaman0362 <zero@362bits.com>
2020-12-26 14:04:46 +0530

    lol

---
+++ dec24/pallindrome.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+#if defined(_WIN32_) || defined(_WIN64_)
+#define clear() system("cls")
+#endif
+#ifdef linux
+#define clear() system("clear")
+#endif
+
+
+int main() {
+
+    const char* method = "string";
+    clear();
+
+    if (method == "integer") {
+        int num, orig, rev = 0;
+        
+        printf("Enter a number : ");
+        scanf("%d", &num);
+        
+        orig = num;
+        if (num < 0) num *= -1;
+        
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        
+        if (rev == orig) printf("%d is a pallindrome.\n", orig);
+    }
+    // Alternatively, using strings
+    else {
+        char str[100];
+        int i = 0, j = 0, isPallindrome = 1;
+        
+        printf("Enter a number / word : ");
+        scanf("%s", str);
+        
+        while (str[i] != '\0') i++;
+        while (i-- >= 0) if (str[i] != str[j++]) isPallindrome = 0;
+
+        if (isPallindrome) printf("%s is a pallindrome.\n", str);
+        else printf("%s is not a pallindrome.\n", str);
+    }
+
+    return 0;
+
+}
---
+++ digital_design/multiplexer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+class Multiplexer {
+public:
+    Multiplexer() {
+        
+    }
+};
