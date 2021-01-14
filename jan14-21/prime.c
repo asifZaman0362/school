6d07e8d1434a22561820bef6ea1dc5e164ff411d
asifZaman0362 <zero362001@gmail.com>
2021-01-14 22:07:16 +0530

    added prime check

---
+++ jan14-21/prime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, isPrime = 1;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            isPrime = 0;
+            break;
+        } else continue;
+    }
+
+    printf("%d is ", num, isPrime ? "a prime." : "not a prime.");
+    
+    return 0;
+}
