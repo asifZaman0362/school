2352345c06647a905fda2a5e093d6ba77d720235
zero362 <zero@362bits.com>
2021-01-15 20:50:47 +0530

    Update hello.c

--- jan14-21/hello.c
+++ jan14-21/hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i < 10; i++) printf("Hello World!\n", str);
+    const char* str = "Hello World!";
+    for (int i = 0; i < 10; i++) printf("%s\n", str);
     return 0;
 }
