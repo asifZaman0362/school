8f18c38ca5ddbf6eed9b117a23876dd8c92a8ac3
asifZaman0362 <zero@362bits.com>
2021-08-01 16:07:00 +0530

    revert because I lost some code

--- sem2/dsa/main.cpp
+++ sem2/dsa/main.cpp
@@ -4,8 +4,11 @@
 
 int main() {
 
+    std::function<bool(int, int)> cmp = [](int x, int y) { return x < y; };
+
     srand(time(0));
     const int n = 100, tc = 50;
+    //const int n = 2000, tc = 500;
     int t = clock();
     int arr[n];
     
@@ -13,7 +16,7 @@ int main() {
     // Merge sort
     for (int i = 0; i < tc; i++) {
         tst::getRandomArray(arr, n);
-        tst::msort(arr, n);
+        tst::msort(arr, n, cmp);
         assert(tst::check_sorted(arr, n));
     }
     std::cout << tc << " merge sort tests passed!\n";
@@ -24,7 +27,7 @@ int main() {
     // Quick sort
     for (int i = 0; i < tc; i++) {
         tst::getRandomArray(arr, n);
-        tst::qsort(arr, 0, n-1);
+        tst::qsort(arr, 0, n-1, cmp);
         assert(tst::check_sorted(arr, n));
     }
     std::cout << tc << " quicksort tests passed!\n";
@@ -35,7 +38,7 @@ int main() {
     // Bubble sort
     for (int i = 0; i < tc; i++) {
         tst::getRandomArray(arr, n);
-        tst::isort(arr, n);
+        tst::isort(arr, n, cmp);
         assert(tst::check_sorted(arr, n));
     }
     std::cout << tc << " insertion sort tests passed!\n";
@@ -47,7 +50,7 @@ int main() {
     tst::getRandomArray(arr, n);
     for (int i = 0; i < tc; i++) {
         tst::getRandomArray(arr, n);
-        tst::ssort(arr, n);
+        tst::ssort(arr, n, cmp);
         assert(tst::check_sorted(arr, n));
     }
     std::cout << tc << " selection sort test passed!\n";
@@ -58,7 +61,7 @@ int main() {
     // Insertion sort
     for (int i = 0; i < tc; i++) {
         tst::getRandomArray(arr, n);
-        tst::bsort(arr, n);
+        tst::bsort(arr, n, cmp);
         assert(tst::check_sorted(arr, n));
     }
     std::cout << tc << " bubble sort tests passed!\n";
