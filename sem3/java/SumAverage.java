4f5cc970b2a21619a7f8a79e5fd895bf26412d54
zero362001 <zero36001@protonmail.com>
2021-12-04 23:09:40 +0530

    Added files

---
+++ sem3/java/SumAverage.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumAverage {
+
+    private static int Sum(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    private static float Average(int x, int y, int z) {
+        return (float)Sum(x, y, z) / 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, n3;
+        System.out.print("Enter the first number: ");
+        n1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        n2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        n3 = sc.nextInt();
+        System.out.println("The sum of the numbers is : " + Sum(n1, n2, n3));
+        System.out.println("The average of the numbers is : " + Average(n1, n2, n3));
+    }
+}
---
+++ sem3/java/VowelCount.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VowelCount {
+    private static int CountVowels(String str) {
+        int count = 0;
+        char[] arr = str.toUpperCase().toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            if (arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U')
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        System.out.println("There are " + CountVowels(str) + " vowels in the string.");
+    }
+}
